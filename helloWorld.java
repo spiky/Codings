ce145d3393d5cb537cdc157a6ace7040cb5e09ef
subratda <subrato.das@oracle.com>
2018-01-14 18:55:37 +0530

    updated helloWorld

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,7 @@
  */
 public class helloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!! ");
-
+        for(int i =1;i<=5;i++)
+        System.out.println(+ i + " : Hello World!! ");
     }
 }
